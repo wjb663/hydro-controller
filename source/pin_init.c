8f05e529f31d9488d0bec44e173b9e071b362a79
wbarret <wbarret@ncsu.edu>
2024-04-05 01:32:28 -0400

    Merged gpio initialization.

--- source/pin_init.c
+++ source/pin_init.c
@@ -3,6 +3,10 @@
 #include "cybsp.h"
 #include "cy_retarget_io.h"
 #include "functions.h"
+#include "macros.h"
+
+cyhal_gpio_callback_data_t gpio_flow_pin_callback_data;
+cyhal_gpio_callback_data_t gpio_temp_pin_callback_data;
 
 void gpio_init()
 {
@@ -26,4 +30,38 @@ void gpio_init()
 	    	printf("ADC initialization failed. Error: %ld\n", (long unsigned int)result);
 	        CY_ASSERT(0);
 	}
+
+	    /* Initialize the User LED */
+    result = cyhal_gpio_init(CYBSP_USER_LED, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, CYBSP_LED_STATE_OFF);
+    /* GPIO init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+     /* Initialize GPIO Flow Sensor Input */
+    result = cyhal_gpio_init(FLOW_PIN, CYHAL_GPIO_DIR_INPUT, CYHAL_GPIO_DRIVE_NONE, 0);
+    /* GPIO init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+     /* Initialize GPIO Temperature Sensor Input/Output */
+    result = cyhal_gpio_init(TEMP_PIN, CYHAL_GPIO_DIR_BIDIRECTIONAL, CYHAL_GPIO_DRIVE_PULLUP, 1);
+    /* GPIO init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    /* Configure GPIO interrupt */
+    // gpio_flow_pin_callback_data.callback = isr_counter;
+    gpio_temp_pin_callback_data.callback = isr_wire;
+    
+    cyhal_gpio_register_callback(FLOW_PIN, &gpio_flow_pin_callback_data);
+    cyhal_gpio_enable_event(FLOW_PIN, CYHAL_GPIO_IRQ_RISE, 7u, true);
+
+    cyhal_gpio_register_callback(TEMP_PIN, &gpio_temp_pin_callback_data);
+    cyhal_gpio_enable_event(TEMP_PIN, CYHAL_GPIO_IRQ_BOTH, 7u, true);
 }
